3179421c2b988f48f3256e6c58ac199d0768ee22
EfeYks534 <yefeyks@gmail.com>
2021-06-02 12:02:31 +0300

    Separated init functions from the kernel

--- src/Kernel.c
+++ src/Kernel.c
@@ -8,27 +8,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef void(*kinit_obj)();
-
-extern kinit_obj keinit_begin;
-extern kinit_obj keinit_end;
-extern kinit_obj klinit_begin;
-extern kinit_obj klinit_end;
-
-void KernelEarlyInit()
-{
-	for(kinit_obj *obj = &keinit_begin; obj < &keinit_end; obj++) {
-		(*obj)();
-	}
-}
-
-void KernelLateInit()
-{
-	for(kinit_obj *obj = &klinit_begin; obj < &klinit_end; obj++) {
-		(*obj)();
-	}
-}
-
 void KernelMain()
 {
 	GDTLoad();
