bd8d7d765a0c3e345511e75bfff3e162d5c38734
EfeYks534 <yefeyks@gmail.com>
2021-06-22 18:13:31 +0300

    Locks now save the interrupt flag

--- src/Lock.c
+++ src/Lock.c
@@ -2,58 +2,51 @@
 #include <Core.h>
 #include <Task.h>
 
-static int saved_ifl[256] = { 0 };
-
 extern int is_panicking;
 
 void Lock(int64_t *lock)
 {
 	if(is_panicking) return;
 
-	saved_ifl[ProcID()] = (FlagsGet() >> 9) & 1;
+	int ifl = (FlagsGet() >> 9) & 1;
 
-	uint8_t type = (*(uint16_t*) lock >> 8) & 0xFF;
+	asm volatile("cli");
 
-	if(type == 0)
-		asm volatile("cli");
+	uint32_t *status = (uint32_t*) lock;
 
-
-	uint8_t e = 0;
+	uint32_t e = 0;
 
 	int r = 0;
 
 	size_t tries = 1000000;
-
-	uint8_t *status = (uint8_t*) lock;
-
 
 	do
 	{
 		r = __atomic_compare_exchange_n(status, &e, 1, 0, __ATOMIC_ACQ_REL, __ATOMIC_ACQUIRE);
 		e = 0;
 
-		if(type == 1)
-			Yield();
-
 		asm volatile("pause");
 	} while(!r && --tries);
 
 	if(tries == 0) {
-		*status = 0;
+		*lock = 0;
 		Panic(NULL, "Deadlocked");
 	}
+
+	uint32_t *higher = (uint32_t*) ((uintptr_t) lock + 4);
+
+	*higher = ifl;
 }
 
 void Unlock(int64_t *lock)
 {
 	if(is_panicking) return;
 
-	uint8_t *status = (uint8_t*) lock;
+	uint32_t *higher = (uint32_t*) ((uintptr_t) lock + 4);
 
-	__atomic_store_n(status, 0, __ATOMIC_RELEASE);
-
-	uint8_t type = (*(uint16_t*) lock >> 8) & 0xFF;
-
-	if(type == 0 && saved_ifl[ProcID()])
+	if(*higher)
 		asm volatile("sti");
+
+	__atomic_store_n(lock, 0, __ATOMIC_RELEASE);
+
 }
